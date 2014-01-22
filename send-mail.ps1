5541748d42a6c18ff27614ab06b3c74cc97411bf
kpietak <kpietak@agh.edu.pl>
2014-01-22 17:17:42 +0100

    Initial commits of send-mail & svg2png scripts.

---
+++ send-mail.ps1
@@ -0,0 +1,42 @@
+﻿# TODO add other options available by Thunderbird or Postbox
+# The full list is available at http://kb.mozillazine.org/Command_line_arguments_(Thunderbird)
+# TODO allow to get many attachments via pipe, eg. ls *.txt | send-mail.ps1
+param(
+    [string]$to="None",
+    [string]$attachment="None",
+    [string]$exec = 'C:\Program Files (x86)\Postbox\postbox.exe'
+    )
+
+$commandBegin = "& `"$exec`" -compose "
+
+
+$params = New-Object Collections.Generic.List[string]
+
+
+if (!($to.Equals("None"))) {
+    $params.Add("to=`'$to`'");
+    
+}
+
+if (!($attachment.Equals("None"))) {
+    if (Test-Path($attachment)) {
+        $fullPath = (Get-Item $attachment).FullName;
+        $params.Add("attachment=`'$fullPath`'"); 
+        
+    
+    } else {
+        echo "The path `"$file`" does not exists."
+    }
+}
+
+$command = $commandBegin;
+
+# TODO: if params list is empty don't place "" in the command
+$command += "`""
+foreach ($param in $params) {
+        $command += $param + ","
+}
+$command += "`""
+
+
+Invoke-Expression $command;
---
+++ svg2png.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [string]$path = '.',
+    [string]$exec = 'C:\Program Files (x86)\Inkscape\inkscape.exe'
+)
+
+foreach ($filename in Get-ChildItem $path) {
+    if ($filename.toString().EndsWith('.svg')) {
+        
+        $targetName = $filename.BaseName + ".png";
+
+        $command = "& `"$exec`" -z -e `"$targetName`" -w 64 `"$filename`""; 
+        Invoke-Expression $command;
+        
+    }
+}
+
+
