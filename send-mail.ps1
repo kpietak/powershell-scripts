8c139b15e6255a7d53cd49d5f3fe13a2e5ead78b
kpietak <kpietak@agh.edu.pl>
2014-01-23 15:14:17 +0100

    [send-mail] handling empty params improved

--- send-mail.ps1
+++ send-mail.ps1
@@ -2,25 +2,25 @@
 # The full list is available at http://kb.mozillazine.org/Command_line_arguments_(Thunderbird)
 # DONE allow to get many attachments via pipe, eg. ls *.txt | send-mail.ps1
 param(
-    [string]$to="None",
-    [string]$attachment="None",
+    [string]$to='',
+    [string]$attachment='',
     [string]$exec = 'C:\Program Files (x86)\Postbox\postbox.exe'
     )
 
 $commandBegin = "& `"$exec`" -compose "
+# ------ Read params from input -------------
 
 
 $params = New-Object Collections.Generic.List[string]
 
-
 # Assign TO parameter
-if (!($to.Equals("None"))) {
+if ($to) {
     $params.Add("to=`'$to`'");
-    
 }
 
 # Assign ATTACHMENTS parameter
-if (!($attachment.Equals("None"))) {
+if ($attachment) {
+
     if (Test-Path($attachment)) {
         $fullPath = (Get-Item $attachment).FullName;
         $params.Add("attachment=`'$fullPath`'"); 
@@ -29,7 +29,6 @@ if (!($attachment.Equals("None"))) {
         echo "The path `"$file`" does not exists."
     }
 } else {
-
     # Try to assign ATTACHMENTS parameter from pipeline 
     $attachParam = "attachment=`'"
     foreach ($filename in $input) {
@@ -41,6 +40,8 @@ if (!($attachment.Equals("None"))) {
     $attachParam += "`'"
     $params.Add($attachParam)
 }
+
+# ----- Build & run command -------------
 
 $command = $commandBegin;
 
