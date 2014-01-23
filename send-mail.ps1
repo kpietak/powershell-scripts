b4906f6119e66dca3e5780f8b0cdef06eb6c8b0e
kpietak <kpietak@agh.edu.pl>
2014-01-23 15:08:37 +0100

    [send-mail] send many attachments via input pipe implemented

--- send-mail.ps1
+++ send-mail.ps1
@@ -1,6 +1,6 @@
 ﻿# TODO add other options available by Thunderbird or Postbox
 # The full list is available at http://kb.mozillazine.org/Command_line_arguments_(Thunderbird)
-# TODO allow to get many attachments via pipe, eg. ls *.txt | send-mail.ps1
+# DONE allow to get many attachments via pipe, eg. ls *.txt | send-mail.ps1
 param(
     [string]$to="None",
     [string]$attachment="None",
@@ -13,20 +13,33 @@ $commandBegin = "& `"$exec`" -compose "
 $params = New-Object Collections.Generic.List[string]
 
 
+# Assign TO parameter
 if (!($to.Equals("None"))) {
     $params.Add("to=`'$to`'");
     
 }
 
+# Assign ATTACHMENTS parameter
 if (!($attachment.Equals("None"))) {
     if (Test-Path($attachment)) {
         $fullPath = (Get-Item $attachment).FullName;
         $params.Add("attachment=`'$fullPath`'"); 
         
-    
     } else {
         echo "The path `"$file`" does not exists."
     }
+} else {
+
+    # Try to assign ATTACHMENTS parameter from pipeline 
+    $attachParam = "attachment=`'"
+    foreach ($filename in $input) {
+         $fullPath = $filename.FullName + ","
+         echo $fullPath
+         $attachParam += $fullPath
+    }
+
+    $attachParam += "`'"
+    $params.Add($attachParam)
 }
 
 $command = $commandBegin;
